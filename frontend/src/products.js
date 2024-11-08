7a2e9afc05a09a4888d191081ae7493f9ae12431
גיל מצפי <gilos@h-mhsb-nyyd-sl-gyl.local>
2024-11-08 15:15:36 +0200

    improve frontend

---
+++ frontend/src/products.js
@@ -0,0 +1,120 @@
+
+const products =
+
+[
+    {
+      "id": 1,
+      "name": "Alexa",
+      "description": "Smart virtual assistant by Amazon for voice-controlled device management.",
+      "price": 99.99,
+      "category": "Smart Home",
+      "image": "/images/alexa.png",
+      "countInStock": 25,
+      "rating": 4.5,
+      "numReviews": 102
+    },
+    {
+      "id": 2,
+      "name": "AirPods",
+      "description": "Wireless earbuds by Apple offering high-quality sound and ease of use.",
+      "price": 199.99,
+      "category": "Audio",
+      "image": "/images/airpods.png",
+      "countInStock": 50,
+      "rating": 4.8,
+      "numReviews": 250
+    },
+    {
+      "id": 3,
+      "name": "Smartphone",
+      "description": "Smartphone combining a high-resolution camera, internet browsing, and various apps.",
+      "price": 599.99,
+      "category": "Mobile",
+      "image": "/images/smartphone.png",
+      "countInStock": 30,
+      "rating": 4.3,
+      "numReviews": 180
+    },
+    {
+      "id": 4,
+      "name": "Smart TV",
+      "description": "Internet-connected television with access to streaming services and additional apps.",
+      "price": 799.99,
+      "category": "Home Entertainment",
+      "image": "/images/smart_tv.png",
+      "countInStock": 15,
+      "rating": 4.7,
+      "numReviews": 95
+    },
+    {
+      "id": 5,
+      "name": "Smart Watch",
+      "description": "Watch with health monitoring, notifications display, and additional functions.",
+      "price": 149.99,
+      "category": "Wearables",
+      "image": "/images/smart_watch.png",
+      "countInStock": 40,
+      "rating": 4.6,
+      "numReviews": 220
+    },
+    {
+      "id": 6,
+      "name": "Security Camera",
+      "description": "Camera designed for monitoring and recording events for security purposes.",
+      "price": 129.99,
+      "category": "Security",
+      "image": "/images/securityCam.png",
+      "countInStock": 20,
+      "rating": 4.4,
+      "numReviews": 80
+    },
+    {
+      "id": 7,
+      "name": "Bluetooth Speaker",
+      "description": "Wireless speaker that connects to other devices via Bluetooth technology.",
+      "price": 59.99,
+      "category": "Audio",
+      "image": "/images/bluetoothRamkol.png",
+      "countInStock": 60,
+      "rating": 4.2,
+      "numReviews": 150
+    },
+    {
+      "id": 8,
+      "name": "Wireless Charger",
+      "description": "Charger that allows wire-free charging for compatible devices.",
+      "price": 39.99,
+      "category": "Accessories",
+      "image": "/images/wireless_charger.png",
+      "countInStock": 75,
+      "rating": 4.1,
+      "numReviews": 60
+    },
+    {
+      "id": 9,
+      "name": "Laptop",
+      "description": "Compact, portable computer allowing work from anywhere.",
+      "price": 899.99,
+      "category": "Computers",
+      "image": "/images/pc.png",
+      "countInStock": 10,
+      "rating": 4.9,
+      "numReviews": 340
+    },
+    {
+      "id": 10,
+      "name": "Soundbar",
+      "description": "Speaker installed below the TV to enhance sound quality.",
+      "price": 199.99,
+      "category": "Home Entertainment",
+      "image": "images/soundbar.png",
+      "countInStock": 18,
+      "rating": 4.6,
+      "numReviews": 90
+    }
+  ]
+
+  export default products;
+  
+  
+  
