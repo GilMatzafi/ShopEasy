e9dac77a5fb40e095e48da9fb21a8ffc29f46d1d
גיל מצפי <gilos@h-mhsb-nyyd-sl-gyl.local>
2024-11-09 12:03:19 +0200

    implement React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import Header from "./components/Header"
-import { Container }  from "react-bootstrap"
-import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet} from 'react-router-dom';
+import { Container }  from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-            <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
