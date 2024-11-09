4139cd94d8aeb0ad4a38f1fe9c1c26e8c9ef13f8
גיל מצפי <gilos@h-mhsb-nyyd-sl-gyl.local>
2024-11-09 12:23:39 +0200

    adding a Rating Component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,31 +1,32 @@
 
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
+import '../assets/styles/Product.css';
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <Link to={`/product/${product.id}`}>
-            <Card.Img src={product.image} variant='top' />
+      <Link to={`/product/${product.id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product.id}`} className="product-title">
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
         </Link>
-    
-        <Card.Body>
-            <Link to={`/product/${product.id}`}>
-            <Card.Title as='div'>
-                <strong>{product.name}</strong>
-            </Card.Title>
-            </Link>
-    
-            <Card.Text as='div'>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
-            </Card.Text>
-    
-            <Card.Text as='h3'>${product.price}</Card.Text>
-        </Card.Body>
+
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;
+
