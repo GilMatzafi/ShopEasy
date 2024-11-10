23af1a19590963636b490c31954aff085829b58f
גיל מצפי <gilos@h-mhsb-nyyd-sl-gyl.local>
2024-11-10 17:25:02 +0200

    cartSlice

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { updateCart} from '../utils/cartUtils';
+
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      // The item to add to the cart
+      const item = action.payload;
+
+      return updateCart(state, item);
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,44 @@
+// Utility function to round numbers to 2 decimal places
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+  
+  export const updateCart = (state,item) => {
+    // Check if the item is already in the cart
+    const existItem = state.cartItems.find((x) => x._id === item._id);
+    
+    if (existItem) {
+        // If it exists, update the quantity
+        state.cartItems = state.cartItems.map((x) =>
+        x._id === existItem._id ? item : x
+        );
+    } else {
+        // If it doesn't exist, add the new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+    }
+    
+    // Calculate item prices
+    state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    );
+    
+    // Calculate shipping price
+    // If items price is greater than 100, shipping is free; otherwise, it's 10
+    state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+    
+    // Calculate tax price at 15% of the items price
+    state.taxPrice = addDecimals(0.15 * state.itemsPrice);
+    
+    // Calculate the total price (items + shipping + tax)
+    state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+    ).toFixed(2);
+    
+    // Save the cart to localStorage
+    localStorage.setItem('cart', JSON.stringify(state));
+
+    return state;
+};
+  
