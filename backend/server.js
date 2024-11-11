995b14b4d557d3f5d2c31704a07098ccc9a694dd
גיל מצפי <gilos@h-mhsb-nyyd-sl-gyl.local>
2024-11-11 14:09:07 +0200

    adding login screen , adding toastify , and improve our api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connecDB from "./config/db.js";
 import ProductRoutes from "./routes/productRoutes.js";
@@ -11,6 +12,11 @@ const port = process.env.PORT || 5001;
 connecDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.use("/api/products", ProductRoutes);
 app.use("/api/users", userRoutes);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import { Outlet} from 'react-router-dom';
-import { Container }  from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
+
       <Header />
       <main className="py-3">
         <Container>
@@ -14,8 +18,8 @@ const App = () => {
         </Container>
       </main>
       <Footer />
-      </>
+    </>
   );
 };
 
-export default App
+export default App;
